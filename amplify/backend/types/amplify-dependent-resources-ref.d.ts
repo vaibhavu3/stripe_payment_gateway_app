cbc427794b9acd4476e3e46b4a1b07b2ad9511a6
vaibhav.u@infogen-labs.com <vaibhav.u@infogen-labs.com>
2022-07-27 03:05:35 -0400

    amplify auth added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "stripepaymentgateway718544e0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
